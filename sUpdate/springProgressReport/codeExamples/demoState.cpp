c35023c75ffe0b7a11156686019488603289c87d
thenellt <thenellt@oregonstate.edu>
2017-05-15 22:54:22 -0700

    messed with code example spacing

--- sUpdate/springProgressReport/codeExamples/demoState.cpp
+++ sUpdate/springProgressReport/codeExamples/demoState.cpp
@@ -1,11 +1,7 @@
 #include "demoState.h"
-
 demo::demo(){}
-
 void demo::handleInput(){}
-
 void demo::runLogic(){}
-
 void demo::drawFrame(){
         disp->scrollingText("Hello ", 0, CRGB::Red, CRGB::Blue);
         disp->scrollingText("World! ", 1, CRGB::Blue, CRGB::Green);
